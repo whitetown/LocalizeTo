54678a527a11c50824766646617c12a07493e942
Sergey Chehuta <whitetownmail@gmail.com>
2019-08-24 08:51:35 +0200

    Initial Commit

---
+++ LocalizeTo/LocalizeTo.h
@@ -0,0 +1,19 @@
+//
+//  LocalizeTo.h
+//  LocalizeTo
+//
+//  Created by Sergey Chehuta on 24/08/2019.
+//  Copyright © 2019 WhiteTown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalizeTo.
+FOUNDATION_EXPORT double LocalizeToVersionNumber;
+
+//! Project version string for LocalizeTo.
+FOUNDATION_EXPORT const unsigned char LocalizeToVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizeTo/PublicHeader.h>
+
+
