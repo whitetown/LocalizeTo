1b736576591728dc62a2a2c41e2e17d2500080a5
Sergey Chehuta <whitetownmail@gmail.com>
2019-10-25 09:03:42 +0200

    fix public

--- LocalizeTo/LocalizeTo.swift
+++ LocalizeTo/LocalizeTo.swift
@@ -399,12 +399,12 @@ extension String {
 
 
     #if DEBUG
-    var unlocalized: String {
+    public var unlocalized: String {
         return self
     }
     #else
     @available(*, deprecated, message: "You should not use unlocalized strings in release builds")
-    var unlocalized: String {
+    public var unlocalized: String {
         return self
     }
     #endif
